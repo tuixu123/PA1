497ffc6ea38c4e197107f38d549fbb103a1d310b
Rayxus <75016723+tuixu123@users.noreply.github.com>
2022-06-25 17:35:52 +0700

    4th commit
    wasted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
-import FeedbackData from "./data/FeedbackData"
+import FeedbackData from "./components/data/FeedbackData"
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)

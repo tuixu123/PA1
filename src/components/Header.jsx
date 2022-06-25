233d3f8f492abb2c9fb609edf0be4bebe9aa2262
Rayxus <75016723+tuixu123@users.noreply.github.com>
2022-06-25 10:59:03 +0700

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,27 @@
+import PropTypes from 'prop-types'
+
+function Header({ text }) {
+  const headerStyles = {
+    backgroundColor: 'blue',
+     color: 'red',
+  }  
+
+  return (
+    <header style={headerStyles}>
+        <div className = "container">
+            <h2>{text}</h2>
+        </div>
+    </header>
+  ) 
+}
+
+Header.defaultProps = {
+    text: 'Feedback UI',
+}
+
+Header.propTypes = {
+    text: PropTypes.string,
+}
+
+export default Header
 

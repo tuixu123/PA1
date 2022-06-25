f9d6d37e710a24048a627fa97bc75b31e6d5f513
Rayxus <75016723+tuixu123@users.noreply.github.com>
2022-06-25 20:05:14 +0700

    5th commit
    done for now

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import { v4 as uuidv4 } from 'uuid'
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./components/data/FeedbackData"
+import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
 
+  const addFeedback =(newFeedback) => {
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+  }
+
+  const deleteFeedback = (id) => {
+    if(window.confirm('are you sure you want to delete')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+    
+  }
   return (
     <>
     <Header  />
       <div className='container'>
-        <FeedbackList feedback={feedback}/>
+        <FeedbackForm handleAdd={addFeedback}/>
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback}
+        handleDelete = {deleteFeedback}/>
       </div>
       </>
   )
